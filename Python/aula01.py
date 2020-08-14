9a46a1dda3d1d1bd17defe6f7e0d7b74c7e31c01
Sabrina Sampaio <47161964+Sabrina-Btt@users.noreply.github.com>
2020-08-14 19:41:58 -0300

    Update aula01.py

--- Python/aula01.py
+++ Python/aula01.py
@@ -129,14 +129,14 @@ pessoa['Idade']=int(input("Digite a idade: "))
 pessoa['Cidade']=input("Digite a cidade: ")
 pessoa['Altura']=float(input("Digite a altura: "))
 
-print(pessoa)'''
+print(pessoa)
 
 
 def imprime(M):
 	print("Matriz 3x3: ")
 	for i in range (0,3):
 		for j in range (0,3):
-			print(M[i][j])
+			print(int(M[i][j]), end='\t')
 		print('')
 
 def insere(M):
@@ -149,20 +149,20 @@ def insere(M):
 	return M
 
 def soma(M,N):
-	M2 =[]
-	for i in range(0,3):
+	M2 =[ ]
+	for i in range(len(M)):
 		L=[]
-		for j in range(0,3):
-			x=M[i][j]+N[i][j]
-			L.append(x)
-		return M2.append(L)
+		for j in range(len(M[0])):
+			L.append(int(M[i][j]+N[i][j]))
+		M2.append(L)
+	return M2
 
 
 def menu():
 	M=[]
 	N=[[int(1),int(2),int(3)],[int(4),int(5),int(6)],[int(7),int(8),int(9)]]
 	op=0
-	while(op!=6):
+	while(op!=4):
 		op=int(input("1- Inserir um elemento na matriz" +
 			         "\n2- Imprimir uma matriz" +
 			         "\n3- Somar matrizes" +
@@ -178,6 +178,100 @@ def menu():
 		
 		
 menu()
+
+
+lista=[0,1,2,3,4]
+print(list(map(lambda x: x+1 if x%2==0 else x,lista)))
+
+
+def inserir():
+	arquivo = open("nomes.txt", 'w')
+	for i in range(0,3):
+	    n=input("Nome: ")
+	    i=input("Idade: ")
+	    s=input("Sexo: ")
+	    print("Nome: " + n + " Idade: " + i + " Sexo: " + s, file=arquivo)
+	arquivo.close()
+
+
+def imprime(idade):
+	arq = open("nomes.txt", 'r')
+	lista=arq.readlines()
+	
+	for i in lista:
+		dados=i.split()
+		if int(dados[3])>idade and dados[5]=='Feminino':
+			print(i)
+	
+	arq.close()
+
+def menu():
+	op=0
+	while (op != 3):
+		op= int(input("1- Inserir dados"+
+			        "\n2- Exibir mulheres maiores de idade"+
+			        "\n3- Sair\n "))
+		if op==1:
+			inserir()
+		elif op==2:
+			idade=int(input("Insira a idade: "))
+			imprime(idade)
+		else:
+			print("Finalizando...")
+
+menu()
+
+
+from functools import reduce
+def verificaL(x,y):
+	return x+y
+
+lista=[1,2,3]
+if reduce(verificaL,lista)==6:
+	print("É número perfeito")
+else:
+	print("Não é numero perfeito")
+
+
+
+
+
+x = int (input("Digite a idade minina da pessoa: "))
+
+import csv
+with open('pessoas.csv','r',encoding='utf-8')as arquivo_csv:
+	leitor = csv.reader(arquivo_csv,delimiter=',')
+	i=0
+	for coluna in leitor:
+		if i == 0:
+			pass
+		elif int(coluna[1])>x:
+			print(coluna)
+		i=i+1
+'''
+
+import json
+with open('teste3.json','r')as json_file:
+	dados=json.load(json_file)
+
+
+for j in range(0,len(dados)):
+	media=0
+	cursos = dados[j]['Curso']
+	pessoas = dados[j]['Alunos']
+	for i in range(0,len(pessoas)):
+		media=media+int(pessoas[i]['idade'])
+
+	media_f=media/len(pessoas)
+	print(media_f)		
+
+
+
+	
+
+
+
+
 
 
 
